798006f6362d9c19efcec832b94673aab77b7eec
bstkt7 <bstkt7@gmail.com>
2026-02-10 19:18:17 +0500

    betaalpha

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,8 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<Price />} />
+          <Route path="/home" element={<Index />} />
           <Route path="/price" element={<Price />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
